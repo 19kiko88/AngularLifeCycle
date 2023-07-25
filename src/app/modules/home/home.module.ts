79a179591e535954953fc70e34b4dfe464de5752
ASUS\Homer_Chen <Homer_Chen@asus.com>
2023-07-25 11:38:57 +0800

    update

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './pages/main/main.component';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
     MainComponent
   ],
   imports: [
-    CommonModule
+    CommonModule, MatTabsModule
   ]
 })
 export class HomeModule { }
